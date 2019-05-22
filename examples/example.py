5dead709f3d6f40dfd1f5409aaac014b9a99e467
Tim Jones <tjones01@gmail.com>
2019-05-22 13:15:32 -0400

    Readme and example.

---
+++ examples/example.py
@@ -0,0 +1,30 @@
+import pycello2.netlist
+import pycello2.ucf
+
+import json
+
+with open('Eco1C1G1T1-synbiohub.UCF.json') as ucf_file:
+    ucf_json = json.load(ucf_file)
+
+with open('and_outputNetlist.json') as netlist_file:
+    netlist_json = json.load(netlist_file)
+
+ucf = pycello2.ucf.UCF(ucf_json)
+netlist = pycello2.netlist.Netlist(netlist_json, ucf)
+
+nodes = [node.name for node in netlist.nodes]
+edges = [(edge.src.name, edge.dst.name) for edge in netlist.edges]
+
+print("nodes: %s\n" % nodes)
+print("edges: %s\n" % edges)
+print("number of placements: %d\n" % len(netlist.placements))
+
+for i, placement in enumerate(netlist.placements):
+    print("placement %d" % i)
+    print("number of sequences/plasmids: %d" % len(placement.groups))
+    for j, group in enumerate(placement.groups):
+        print("\tsequence/plasmid: %d" % j)
+        print("\ttranscriptional units: %d" % len(group.components))
+        for k, component in enumerate(group.components):
+            print("\t\tcomponent: %s" % component.name)
+    print()
