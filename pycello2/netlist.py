eba6741da143d9d62c5619420a046f772efb8eda
Tim Jones <tjones01@gmail.com>
2019-12-12 17:21:37 -0500

    fromLogicCircuit classmethod in Netlist.

--- pycello2/netlist.py
+++ pycello2/netlist.py
@@ -1,3 +1,5 @@
+import re
+
 __author__ = 'Timothy S. Jones <jonests@bu.edu>, Densmore Lab, BU'
 __license__ = 'GPL3'
 
@@ -6,8 +8,9 @@ class Node:
 
     def __init__(self, node, ucf):
         self.name = node['name']
-        self.gate = ucf.gate(node['gateType'])
         self.type = node['nodeType']
+        self.partition_id = node['partitionID']
+        self.gate = ucf.gate(node['gateType'])
 
     @property
     def name(self):
@@ -18,6 +21,22 @@ class Node:
         self.__name = name
 
     @property
+    def type(self):
+        return self.__type
+
+    @type.setter
+    def type(self, type):
+        self.__type = type
+
+    @property
+    def partition_id(self):
+        return self.__partition_id
+
+    @partition_id.setter
+    def partition_id(self, partition_id):
+        self.__partition_id = partition_id
+
+    @property
     def gate(self):
         return self.__gate
 
@@ -26,12 +45,15 @@ class Node:
         self.__gate = gate
 
     @property
-    def type(self):
-        return self.__type
+    def json(self):
+        rtn = {
+            "name": self.name,
+            "nodeType": self.type,
+            "partitionID": self.partition_id,
+            "gateType": self.gate.name
+        }
 
-    @type.setter
-    def type(self, type):
-        self.__type = type
+        return rtn
 
 
 class Edge:
@@ -62,6 +84,16 @@ class Edge:
     @dst.setter
     def dst(self, dst):
         self.__dst = dst
+
+    @property
+    def json(self):
+        rtn = {
+            "name": self.name,
+            "src": self.src.name,
+            "dst": self.dst.name
+        }
+
+        return rtn
 
 
 class Placement:
@@ -171,6 +203,8 @@ class PartInstance:
 class Netlist:
 
     def __init__(self, netlist, ucf):
+        self.name = netlist['name'] if 'name' in netlist else ""
+        self.input_filename = netlist['inputFilename'] if 'inputFilename' in netlist else ""
         self.nodes = []
         self.edges = []
         self.placements = []
@@ -205,6 +239,81 @@ class Netlist:
                                 instance = PartInstance(part)
                                 c.parts.append(instance)
 
+    @classmethod
+    def fromLogicCircuit(self, lc, ucf):
+        netlist = {}
+        netlist["name"] = "default"
+        netlist["inputFilename"] = "default.v"
+        netlist["nodes"] = []
+        netlist["edges"] = []
+        netlist["placements"] = []
+
+        nodes = {}
+        edges = []
+
+        for line in lc:
+            regex = r"^(OUTPUT_OR|OUTPUT|NOT|NOR|INPUT)\s+[01]+\s+(\w+)\s+(\d+)\s+(\(\d+(,\d+)*\))?"
+            m = re.match(regex, line)
+            if (m):
+                node = {}
+                node["name"] = m.group(2)
+                if (m.group(1) in ["OUTPUT_OR", "OUTPUT"]):
+                    node["nodeType"] = "PRIMARY_OUTPUT"
+                    node["gateType"] = re.sub("^output_", "", m.group(2))
+                    node["gateType"] += "_reporter"
+                elif (m.group(1) == "INPUT"):
+                    node["nodeType"] = "PRIMARY_INPUT"
+                    node["gateType"] = re.sub("^input_", "", m.group(2))
+                    if (node["gateType"] == "pBAD"):
+                        node["gateType"] = "AraC_sensor"
+                    if (node["gateType"] == "pTet"):
+                        node["gateType"] = "TetR_sensor"
+                    if (node["gateType"] == "pLuxStar"):
+                        node["gateType"] = "LuxR_sensor"
+                    if (node["gateType"] == "pTac"):
+                        node["gateType"] = "LacI_sensor"
+                else:
+                    node["nodeType"] = m.group(1)
+                    node["gateType"] = m.group(2)
+
+                node["partitionID"] = -1
+
+                nodes[m.group(3)] = node["name"]
+
+                netlist["nodes"].append(node)
+
+                if (m.group(4)):
+                    inputs = m.group(4)
+                    inputs = inputs.lstrip("(")
+                    inputs = inputs.rstrip(")")
+                    for dst in inputs.split(","):
+                        edges.append((dst, m.group(3)))
+
+        for edge in edges:
+            netlist_edge = {}
+            netlist_edge["name"] = nodes[edge[0]] + "_" + nodes[edge[1]]
+            netlist_edge["src"] = nodes[edge[0]]
+            netlist_edge["dst"] = nodes[edge[1]]
+            netlist["edges"].append(netlist_edge)
+
+        return Netlist(netlist, ucf)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def input_filename(self):
+        return self.__input_filename
+
+    @input_filename.setter
+    def input_filename(self, input_filename):
+        self.__input_filename = input_filename
+
     @property
     def nodes(self):
         return self.__nodes
@@ -233,3 +342,22 @@ class Netlist:
     @placements.setter
     def placements(self, placements):
         self.__placements = placements
+
+    @property
+    def json(self):
+        rtn = {
+            "name": self.name,
+            "inputFilename": self.input_filename,
+            "placements": [],
+            "nodes": [],
+            "edges": []
+        }
+
+        for placement in self.placements:
+            pass
+        for node in self.nodes:
+            rtn['nodes'].append(node.json)
+        for edge in self.edges:
+            rtn['edges'].append(edge.json)
+
+        return rtn
