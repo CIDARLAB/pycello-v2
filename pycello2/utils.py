92675f47b490ba04093bf975b84dfe0c66044990
Tim Jones <tjones01@gmail.com>
2019-12-21 21:10:26 -0500

    Update docstrings and imports in pycello2.utils.

--- pycello2/utils.py
+++ pycello2/utils.py
@@ -1,37 +1,88 @@
-from . import netlist as pycello2_netlist
+from pycello2.netlist import Component, Node, Netlist, Placement
+from pycello2.ucf import Gate, Part
 import sympy
 
 __author__ = 'Timothy S. Jones <jonests@bu.edu>, Densmore Lab, BU'
 __license__ = 'GPL3'
 
 
-def get_upstream_node(part,
-                      node: pycello2_netlist.Node,
-                      netlist: pycello2_netlist.Netlist):
-    """Get the node immediately upstream of a given node, with gate having
-    promoter 'part'."""
+def get_upstream_nodes(node: Node, netlist: Netlist):
+    """Get the nodes connected as inputs to the given node in the netlist.
+
+    Parameters
+    ----------
+    node : pycello2.netlist.Node
+    netlist : pycello2.netlist.Netlist
+
+    """
+    upstream = []
+    for edge in netlist.edges:
+        if edge.dst == node:
+            upstream.append(edge.src)
+    return upstream
+
+
+def get_upstream_node(promoter: Part, node: Node, netlist: Netlist):
+    """Get the node with a given promoter connected as input to the given node.
+
+    Parameters
+    ----------
+    promoter : pycello2.ucf.Part
+    node : pycello2.netlist.Node
+    netlist : pycello2.netlist.Netlist
+
+    """
     for upstream in get_upstream_nodes(node, netlist):
         gate = upstream.gate
-        if gate.promoter == part:
+        if gate.promoter == promoter:
             return upstream
 
 
-def get_cds(component: pycello2_netlist.Component):
-    """Get the coding sequence in a given component."""
+def get_cds(component: Component):
+    """Get the coding sequence in a given component.
+
+    Parameters
+    ----------
+    component : pycello2.netlist.Component
+
+    Notes
+    -----
+    If more than one coding sequence is present, only the first in the
+    `component` will be returned.
+
+    """
     for part_instance in component.parts:
         if part_instance.part.type == 'cds':
             return part_instance
 
 
-def get_ribozyme(component: pycello2_netlist.Component):
-    """Get the ribozyme sequence in a given component."""
+def get_ribozyme(component: Component):
+    """Get the ribozyme in a given component.
+
+    Parameters
+    ----------
+    component : pycello2.netlist.Component
+
+    Notes
+    -----
+    If more than one ribozyme is present, only the first in the
+    `component` will be returned.
+
+    """
     for part_instance in component.parts:
         if part_instance.part.type == 'ribozyme':
             return part_instance.part
 
 
-def get_components(node: pycello2_netlist.Node, placement: pycello2_netlist.Placement):
-    """Get the components in a placement that correspond to a given netlist node."""
+def get_components(node: Node, placement: Placement):
+    """Get the components in a placement corresponding to a given netlist node.
+
+    Parameters
+    ----------
+    node : pycello2.netlist.Node
+    placement : pycello2.netlist.Placement
+
+    """
     components = []
     for group in placement.groups:
         for component in group.components:
@@ -40,16 +91,15 @@ def get_components(node: pycello2_netlist.Node, placement: pycello2_netlist.Plac
     return components
 
 
-def get_upstream_nodes(node: pycello2_netlist.Node, netlist: pycello2_netlist.Netlist):
-    """Get the nodes immediately upstream of a given node."""
-    upstream = []
-    for edge in netlist.edges:
-        if edge.dst == node:
-            upstream.append(edge.src)
-    return upstream
+def evaluate_equation(gate: Gate, variables: dict):
+    """Evaluate a gate's response function with supplied variables.
 
+    Parameters
+    ----------
+    gate : pycello2.ucf.Gate
+    variables: dict
 
-def evaluate_equation(gate, variables):
+    """
     param_str = ""
     for param in gate.parameters.keys():
         param_str += param + " "
