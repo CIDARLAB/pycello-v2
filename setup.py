acef9b6fd7630969d01b7c7b76b87b4d1efb78e4
Timothy Jones <jonests@bu.edu>
2020-03-04 10:23:48 -0500

    setup.py updates.

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ def read(fname):
 
 setup(
     name = "pycello",
-    version = "0.0.1",
+    version = "2.0.0",
     author = "Timothy Jones",
-    author_email = "tjones01@gmail.com",
-    description = "A Python module for processing Cello2 inputs and outputs, namely the netlist and UCF.",
+    author_email = "jonests@bu.edu",
+    description = "A Python module for processing Cello v2 inputs and outputs, namely the netlist and UCF.",
     license = "GPLv3",
     keywords = "cello cellular-logic synthetic-biology",
-    url = "https://github.com/tim-tx/pycello",
+    url = "https://github.com/CIDARLAB/pycello-v2",
     packages = find_packages(),
     install_requires = [
         "sympy>=1.5",
