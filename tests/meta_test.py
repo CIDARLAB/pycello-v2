86d7b6d8543b813953044f66c1a504ab4e350074
Timothy Jones <jonests@bu.edu>
2020-03-05 11:32:12 -0500

    Test all standard UCFs.

---
+++ tests/meta_test.py
@@ -0,0 +1,32 @@
+import json
+
+
+__author__ = 'Timothy S. Jones <jonests@bu.edu>, Densmore Lab, BU'
+__license__ = 'GPL3'
+
+
+def get_json_file_contents(file_name):
+    with open(file_name) as f:
+        obj = json.load(f)
+    return obj
+
+
+class TestFileMeta(type):
+    def __new__(cls, name, bases, attrs):
+        def create_func(test_name, file_name, test_func):
+            def func(self):
+                test_func(self, file_name)
+            func.__name__ = "test_{}_{}".format(str(test_name), attr[6:])
+            return func
+
+        args = attrs['get_test_args']
+        for test_name, file_name in args():
+            d = {}
+            for attr in attrs:
+                if attr.startswith("_test_"):
+                    func = create_func(test_name, file_name, attrs[attr])
+                    d[func.__name__] = func
+            for key in d:
+                attrs[key] = d[key]
+
+        return type.__new__(cls, name, bases, attrs)
--- tests/test_user_constraints_file.py
+++ tests/test_user_constraints_file.py
@@ -1,18 +1,24 @@
+import unittest
+from glob import glob
+from os.path import basename
 from .context import pycello
 import pycello.target_data
-import json
-import unittest
+from .meta_test import TestFileMeta, get_json_file_contents
 
 __author__ = 'Timothy S. Jones <jonests@bu.edu>, Densmore Lab, BU'
 __license__ = 'GPL3'
 
 
-class TestUserConstraintsFile(unittest.TestCase):
+class TestUserConstraintsFile(unittest.TestCase, metaclass=TestFileMeta):
 
-    def test_user_constraints_file(self):
-        with open('examples/Cello-UCF/files/v2/ucf/Eco/Eco1C1G1T1.UCF.json') as ucf_file:
-            ucf = json.load(ucf_file)
-        ucf = pycello.target_data.UserConstraintsFile(ucf)
+    def get_test_args():
+        files = glob("examples/Cello-UCF/files/v2/ucf/**/*.UCF.json", recursive=True)
+        for f in files:
+            yield (basename(f), f)
+
+    def _test_user_constraints_file(self, f):
+        ucf = get_json_file_contents(f)
+        pycello.target_data.UserConstraintsFile(ucf)
 
 
 if __name__ == '__main__':
